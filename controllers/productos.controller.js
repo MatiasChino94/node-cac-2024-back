8b514ad992d867ac673b38418ad41bcb0a1dff47
MatiasMartinez <matiaschino2012@gmail.com>
2024-07-03 23:36:03 -0300

    clase 6 node multer

--- controllers/productos.controller.js
+++ controllers/productos.controller.js
@@ -111,5 +111,4 @@ const borrarProducto = (req, res) => {
         crearProducto,
         modificarProductos, 
         borrarProducto
-
     }
--- routes/productos.router.js
+++ routes/productos.router.js
@@ -1,6 +1,51 @@
 const express = require('express');
 const router = express.Router();
 
+//configurando multer
+const multer = require('multer');
+const path = require('path');
+
+//creamos un storage, un lugar donde guardar el archivo cuando se suba
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb( null, 'uploads' )
+    },
+    filename:(req, file, cb) =>{
+        console.log(file);
+        cb(null, Date.now() + path.extname(file.originalname))
+    }
+});
+
+//esto para que sepas donde los vas a guardar
+//y validar los archivos y peso que mandamos 
+const upload = multer({ 
+    storage,
+    
+    fileFilter: (req, file, cb) => {
+        //expresion regular 
+        const filetypes = /jpeg|jpg|png/;
+
+        //esto retorna true o false
+        const mimetype = filetypes.test(file.mimetype);
+
+        const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+
+        if(mimetype && extname){
+            return cb(null,true);
+        }
+
+        cb('tipo de archivo no soportado');
+    },
+
+    limits: {fileSize: 1024 * 1024 * 1},
+
+
+});
+
+//esto es para agregar una imagen a la tabla 
+//router.post('/', upload.single('imagen'), controller.crearProducto);
+
+
 const controller = require('../controllers/productos.controller');
 
 
@@ -42,59 +87,59 @@ router.delete('/:id', controller.borrarProducto);
 
 //se envia un cuerpo a esta peticion 
 //creamos un elemento atraves del metodo POST
-router.post('/', (req, res) => {
+// router.post('/', (req, res) => {
 
-    console.log(req.body);
-    const productoEnviado = req.body;
+//     console.log(req.body);
+//     const productoEnviado = req.body;
 
-    const producto = {
-        id: productos.length + 1,
-        nombre: productoEnviado.nombre,
-        stock: productoEnviado.stock
-    };
+//     const producto = {
+//         id: productos.length + 1,
+//         nombre: productoEnviado.nombre,
+//         stock: productoEnviado.stock
+//     };
 
-    productos.push(producto);
+//     productos.push(producto);
 
-    res.send(producto);
-});
+//     res.send(producto);
+// });
 
 //metodo PUT
 //Actualizar datos
-router.put('/:id', (req, res) => {
-    console.log(req.params);
-    console.log(req.body);
+// router.put('/:id', (req, res) => {
+//     console.log(req.params);
+//     console.log(req.body);
 
-    const producto = productos.find((e) => e.id == req.params.id);
+//     const producto = productos.find((e) => e.id == req.params.id);
 
-    if(!producto){
-        return res.status(404).json({error:'No existe el producto'})
-    }
+//     if(!producto){
+//         return res.status(404).json({error:'No existe el producto'})
+//     }
 
-    producto.nombre = req.body.nombre;
-    producto.stock = req.body.stock; 
+//     producto.nombre = req.body.nombre;
+//     producto.stock = req.body.stock; 
 
-    res.send(producto)
-});
+//     res.send(producto)
+// });
 
 
 //metodo DELETE
 //BORRAR DATOS
-router.delete('/:id' , (req, res) => {
+// router.delete('/:id' , (req, res) => {
 
-    const producto = productos.find((e) => e.id == req.params.id);
+//     const producto = productos.find((e) => e.id == req.params.id);
 
-    if(!producto){
-        return res.status(404).json({error:'No existe el producto'});
-    }
+//     if(!producto){
+//         return res.status(404).json({error:'No existe el producto'});
+//     }
 
-    const productoIndex = productos.findIndex(e => e.id == req.params);
+//     const productoIndex = productos.findIndex(e => e.id == req.params);
 
-    console.log(productoIndex);
+//     console.log(productoIndex);
 
-    productos.splice(productoIndex, 1);
+//     productos.splice(productoIndex, 1);
 
-    res.send(producto)
-})
+//     res.send(producto)
+// })
 
 
 
